de90e7512d6bb04e0f6d0764d4277156b5bb8d3f
Ryan Bigg <radarlistener@gmail.com>
2009-08-08 21:09:15 +1000

    Minus a day if largest hour is greater than smallest hour (range goes over a day "edge")

--- lib/dotiw.rb
+++ lib/dotiw.rb
@@ -5,6 +5,7 @@ module ActionView
         output = HashWithIndifferentAccess.new
         from_time = from_time.to_time if from_time.respond_to?(:to_time)
         to_time = to_time.to_time if to_time.respond_to?(:to_time)
+        
         distance = (from_time - to_time).abs
         I18n.with_options :locale => options[:locale] do |locale|
           while distance > 0
@@ -25,6 +26,9 @@ module ActionView
               years, months = months.divmod(12)
             
               days = largest.day - smallest.day
+              
+              # Will otherwise incorrectly say one more day if our range goes over a day. 
+              days -= 1 if largest.hour < smallest.hour
             
               if days < 0
                 # Convert the last month to days and add to total
