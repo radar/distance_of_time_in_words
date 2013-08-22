ecd7727680b4895fbe412f7ca04fb90f7f9fe92c
Florian Hintermeier <florian@hintermeier.at>
2013-08-22 22:32:29 +0200

    added a helper method to choose between new and old behaviour - old: singularize keys - new: use x_years with one/other

--- lib/dotiw.rb
+++ lib/dotiw.rb
@@ -13,7 +13,7 @@ module ActionView
 
       def distance_of_time_in_words_hash(from_time, to_time, options = {})
         from_time = from_time.to_time if !from_time.is_a?(Time) && from_time.respond_to?(:to_time)
-        to_time   = to_time.to_time   if !to_time.is_a?(Time)   && to_time.respond_to?(:to_time)
+        to_time = to_time.to_time if !to_time.is_a?(Time) && to_time.respond_to?(:to_time)
 
         DOTIW::TimeHash.new((from_time - to_time).abs, from_time, to_time, options).to_hash
       end
@@ -45,53 +45,54 @@ module ActionView
 
 
       private
-        def display_time_in_words(hash, options = {})
-          options = {
+      def display_time_in_words(hash, options = {})
+        options = {
             :include_seconds => false
-          }.update(options).symbolize_keys
+        }.update(options).symbolize_keys
 
-          I18n.locale = options[:locale] if options[:locale]
+        I18n.locale = options[:locale] if options[:locale]
 
-          time_measurements = ActiveSupport::OrderedHash.new
-          time_measurements[:years]   = DOTIW::Helper::i18n_t(:years)
-          time_measurements[:months]  = DOTIW::Helper::i18n_t(:months)
-          time_measurements[:weeks]   = DOTIW::Helper::i18n_t(:weeks)
-          time_measurements[:days]    = DOTIW::Helper::i18n_t(:days)
-          time_measurements[:hours]   = DOTIW::Helper::i18n_t(:hours)
-          time_measurements[:minutes] = DOTIW::Helper::i18n_t(:minutes)
-          time_measurements[:seconds] = DOTIW::Helper::i18n_t(:seconds)
+        time_measurements = ActiveSupport::OrderedHash.new
+        time_measurements[:years] = DOTIW::Helper::i18n_t(:years)
+        time_measurements[:months] = DOTIW::Helper::i18n_t(:months)
+        time_measurements[:weeks] = DOTIW::Helper::i18n_t(:weeks)
+        time_measurements[:days] = DOTIW::Helper::i18n_t(:days)
+        time_measurements[:hours] = DOTIW::Helper::i18n_t(:hours)
+        time_measurements[:minutes] = DOTIW::Helper::i18n_t(:minutes)
+        time_measurements[:seconds] = DOTIW::Helper::i18n_t(:seconds)
 
-          hash.delete(time_measurements[:seconds]) if !options.delete(:include_seconds) && hash[time_measurements[:minutes]]
+        hash.delete(time_measurements[:seconds]) if !options.delete(:include_seconds) && hash[time_measurements[:minutes]]
 
-          # Remove all the values that are nil or excluded. Keep the required ones.
-          time_measurements.delete_if do |measure, key|
-            hash[key].nil? || hash[key].zero? || (!options[:except].nil? && options[:except].include?(key)) ||
+        # Remove all the values that are nil or excluded. Keep the required ones.
+        time_measurements.delete_if do |measure, key|
+          hash[key].nil? || hash[key].zero? || (!options[:except].nil? && options[:except].include?(key)) ||
               (options[:only] && !options[:only].include?(key))
-          end
-
-          options.delete(:except)
-          options.delete(:only)
-
-          output = []
-
-          time_measurements = Hash[*time_measurements.first] if options.delete(:highest_measure_only)
-
-          time_measurements.each do |measure, key|
-
-            name = options[:singularize] == :always || hash[key].between?(-1, 1) ? key.singularize : key
-            output += ["#{hash[key]} #{name}"]
-          end
-
-          options.delete(:singularize)
-
-          # maybe only grab the first few values
-          if options[:precision]
-            output = output[0...options[:precision]]
-            options.delete(:precision)
-          end
-
-          output.to_sentence(options)
         end
+
+        options.delete(:except)
+        options.delete(:only)
+
+        output = []
+
+        time_measurements = Hash[*time_measurements.first] if options.delete(:highest_measure_only)
+
+        time_measurements.each do |measure, key|
+          # TODO: make sure old behavior is still working
+          value = hash[key]
+          name = options[:singularize] == :always || value.between?(-1, 1) ? DOTIW::Helper::singularize(measure, key) : DOTIW::Helper::pluralize(measure, key)
+          output += ["#{hash[key]} #{name}"]
+        end
+
+        options.delete(:singularize)
+
+        # maybe only grab the first few values
+        if options[:precision]
+          output = output[0...options[:precision]]
+          options.delete(:precision)
+        end
+
+        output.to_sentence(options)
+      end
     end # DateHelper
   end # Helpers
 end # ActionView
--- lib/dotiw/helper.rb
+++ lib/dotiw/helper.rb
@@ -5,13 +5,48 @@
 module DOTIW
 
   class Helper
-    def self.i18n_t(name, fallback = nil)
 
+    # simply choose the translation tag with a default
+    def self.i18n_t(name, fallback = nil)
       # set fallback if empty
       # assume name is a symbol and set string
       dfallback = name.to_s if fallback.nil?
       return I18n.t(name, :default => fallback)
     end
-  end
 
+    def self.singular_plural_key(measure, key, count = 1)
+      #first: check if there is a x_key one/other styled key
+      x_key = "x_#{measure.to_s}".to_sym
+      if exist_x_key? x_key
+
+        return I18n.t x_key, :count => count
+      else
+        if count == 1
+          # old DOTIW behaviour: defaults to key.singularize
+          return key.singularize
+        else
+          # old DOTIW behaviour: suppose that key is aldready in plural
+          return key
+        end
+      end
+    end
+
+    def self.singularize( measure, key)
+      return self.singular_plural_key measure, key, count = 1
+    end
+
+    def self.pluralize( measure, key)
+      return self.singular_plural_key measure, key, count = 2
+    end
+
+    def self.exist_x_key?(key)
+      bla = I18n.t(x_key, :default => '', :raise => true) rescue false
+      if bla.blank?
+        return false
+      else
+        return true
+      end
+    end
+
+  end
 end
