56ce79cd76cd5de6917b0165f648a90bc6d1f5fa
Sijawusz Pur Rahnama <sija@sija.pl>
2013-09-12 04:24:22 +0200

    Indent nazi

--- lib/dotiw/time_hash.rb
+++ lib/dotiw/time_hash.rb
@@ -21,84 +21,84 @@ module DOTIW
       output
     end
 
-    private
-      attr_accessor :options, :output
+  private
+    attr_accessor :options, :output
 
-      def build_time_hash
-        if accumulate_on = options.delete(:accumulate_on)
-          return build_time_hash if accumulate_on == :years
-          TIME_FRACTIONS.index(accumulate_on).downto(0) { |i| self.send("build_#{TIME_FRACTIONS[i]}") }
-        else
-          while distance > 0
-            if distance < 1.minute
-              build_seconds
-            elsif distance < 1.hour
-              build_minutes
-            elsif distance < 1.day
-              build_hours
-            elsif distance < 28.days
-              build_days
-            else # greater than a month
-              build_years_months_days
-            end
+    def build_time_hash
+      if accumulate_on = options.delete(:accumulate_on)
+        return build_time_hash if accumulate_on == :years
+        TIME_FRACTIONS.index(accumulate_on).downto(0) { |i| self.send("build_#{TIME_FRACTIONS[i]}") }
+      else
+        while distance > 0
+          if distance < 1.minute
+            build_seconds
+          elsif distance < 1.hour
+            build_minutes
+          elsif distance < 1.day
+            build_hours
+          elsif distance < 28.days
+            build_days
+          else # greater than a month
+            build_years_months_days
           end
         end
-
-        output
       end
 
-      def build_seconds
-        output[:seconds] = distance.to_i
-        self.distance = 0
+      output
+    end
+
+    def build_seconds
+      output[:seconds] = distance.to_i
+      self.distance = 0
+    end
+
+    def build_minutes
+      output[:minutes], self.distance = distance.divmod(1.minute)
+    end
+
+    def build_hours
+      output[:hours], self.distance = distance.divmod(1.hour)
+    end
+
+    def build_days
+      output[:days], self.distance = distance.divmod(1.day)
+    end
+
+    def build_months
+      build_years_months_days
+
+      if (years = output.delete(:years)) > 0
+        output[:months] += (years * 12)
+      end
+    end
+
+    def build_years_months_days
+      months = (largest.year - smallest.year) * 12 + (largest.month - smallest.month)
+      years, months = months.divmod(12)
+
+      days = largest.day - smallest.day
+
+      # Will otherwise incorrectly say one more day if our range goes over a day.
+      days -= 1 if largest.hour < smallest.hour
+
+      if days < 0
+        # Convert the last month to days and add to total
+        months -= 1
+        last_month = largest.advance(:months => -1)
+        days += Time.days_in_month(last_month.month, last_month.year)
       end
 
-      def build_minutes
-        output[:minutes], self.distance = distance.divmod(1.minute)
+      if months < 0
+        # Convert a year to months
+        years -= 1
+        months += 12
       end
 
-      def build_hours
-        output[:hours], self.distance = distance.divmod(1.hour)
-      end
+      output[:years]   = years
+      output[:months]  = months
+      output[:days]    = days
 
-      def build_days
-        output[:days], self.distance = distance.divmod(1.day)
-      end
-
-      def build_months
-        build_years_months_days
-
-        if (years = output.delete(:years)) > 0
-          output[:months] += (years * 12)
-        end
-      end
-
-      def build_years_months_days
-        months = (largest.year - smallest.year) * 12 + (largest.month - smallest.month)
-        years, months = months.divmod(12)
-
-        days = largest.day - smallest.day
-
-        # Will otherwise incorrectly say one more day if our range goes over a day.
-        days -= 1 if largest.hour < smallest.hour
-
-        if days < 0
-          # Convert the last month to days and add to total
-          months -= 1
-          last_month = largest.advance(:months => -1)
-          days += Time.days_in_month(last_month.month, last_month.year)
-        end
-
-        if months < 0
-          # Convert a year to months
-          years -= 1
-          months += 12
-        end
-
-        output[:years]   = years
-        output[:months]  = months
-        output[:days]    = days
-
-        total_days, self.distance = (from_time - to_time).abs.divmod(1.day)
-      end
+      total_days, self.distance = (from_time - to_time).abs.divmod(1.day)
+    end
   end # TimeHash
 end # DOTIW
