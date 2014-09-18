e21bd2a418e0a597d7c229a5245a591902a6f522
Sijawusz Pur Rahnama <sija@sija.pl>
2014-09-18 10:03:02 +0200

    Remove obsolete comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,9 @@ require 'erb'
 require 'active_support/all'
 require 'action_view'
 
-require 'dotiw' # require dotiw through init (like a plugin would)
+require 'dotiw'
 
 Time.zone = 'UTC'
-# I18n.load_path.clear
-I18n.load_path << Dir[File.join(File.dirname(__FILE__), "translations", "*")]
+
+I18n.load_path << Dir[File.join(File.dirname(__FILE__), 'translations', '*')]
 I18n.locale = :en
