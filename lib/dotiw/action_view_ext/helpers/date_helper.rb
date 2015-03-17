12b118d654ad32579f49a47675ff3b35ef477dfe
Roy <roy.vanenk@censio-it.com>
2015-03-17 12:40:58 +0100

    Fixed issue #50. 'Less than X' output now respects :except and :only options.

--- lib/dotiw/action_view_ext/helpers/date_helper.rb
+++ lib/dotiw/action_view_ext/helpers/date_helper.rb
@@ -5,7 +5,7 @@ module ActionView
 
       def distance_of_time_in_words_hash(from_time, to_time, options = {})
         from_time = from_time.to_time if !from_time.is_a?(Time) && from_time.respond_to?(:to_time)
-        to_time   = to_time.to_time   if !to_time.is_a?(Time)   && to_time.respond_to?(:to_time)
+        to_time = to_time.to_time if !to_time.is_a?(Time) && to_time.respond_to?(:to_time)
 
         DOTIW::TimeHash.new(nil, from_time, to_time, options).to_hash
       end
@@ -40,35 +40,46 @@ module ActionView
         distance_of_time_in_words(from_time, Time.now, include_seconds_or_options)
       end
 
-    private
+      private
       def display_time_in_words(hash, options = {})
         options.reverse_merge!(
-          :include_seconds => false
+            :include_seconds => false
         ).symbolize_keys!
 
         include_seconds = options.delete(:include_seconds)
         hash.delete(:seconds) if !include_seconds && hash[:minutes]
 
-        options[:except] = Array.wrap(options[:except]).map!(&:to_s) if options[:except]
-        options[:only] = Array.wrap(options[:only]).map!(&:to_s) if options[:only]
+        options[:except] = Array.wrap(options[:except]).map!(&:to_sym) if options[:except]
+        options[:only] = Array.wrap(options[:only]).map!(&:to_sym) if options[:only]
 
         # Remove all the values that are nil or excluded. Keep the required ones.
         hash.delete_if do |key, value|
           value.nil? || value.zero? ||
-            (options[:except] && options[:except].include?(key.to_s)) ||
-            (options[:only] && !options[:only].include?(key.to_s))
+              (options[:except] && options[:except].include?(key)) ||
+              (options[:only] && !options[:only].include?(key))
         end
-        return I18n.t('datetime.distance_in_words.less_than_x_seconds', :count => 1, :locale => options[:locale]) if hash.empty?
-
-        options.delete(:except)
-        options.delete(:only)
 
         i18n_scope = options.delete(:scope) || DOTIW::DEFAULT_I18N_SCOPE
+        if hash.empty?
+          fractions = DOTIW::TimeHash::TIME_FRACTIONS
+          fractions = fractions & options[:only] if options[:only]
+          fractions = fractions - options[:except] if options[:except]
+
+          I18n.with_options :locale => options[:locale], :scope => i18n_scope do |locale|
+            # e.g. try to format 'less than 1 days', fallback to '0 days'
+            return locale.translate :less_than_x,
+                                    :distance => locale.translate(fractions.first, :count => 1),
+                                    :default => locale.translate(fractions.first, :count => 0)
+          end
+        end
+
         output = []
         I18n.with_options :locale => options[:locale], :scope => i18n_scope do |locale|
           output = hash.map { |key, value| locale.t(key, :count => value) }
         end
 
+        options.delete(:except)
+        options.delete(:only)
         highest_measures = options.delete(:highest_measures)
         highest_measures = 1 if options.delete(:highest_measure_only)
         if highest_measures
@@ -76,14 +87,14 @@ module ActionView
         end
 
         options[:words_connector] ||= I18n.translate :'datetime.dotiw.words_connector',
-          :default => :'support.array.words_connector',
-          :locale => options[:locale]
+                                                     :default => :'support.array.words_connector',
+                                                     :locale => options[:locale]
         options[:two_words_connector] ||= I18n.translate :'datetime.dotiw.two_words_connector',
-          :default => :'support.array.two_words_connector',
-          :locale => options[:locale]
+                                                         :default => :'support.array.two_words_connector',
+                                                         :locale => options[:locale]
         options[:last_word_connector] ||= I18n.translate :'datetime.dotiw.last_word_connector',
-          :default => :'support.array.last_word_connector',
-          :locale => options[:locale]
+                                                         :default => :'support.array.last_word_connector',
+                                                         :locale => options[:locale]
 
         output.to_sentence(options)
       end
